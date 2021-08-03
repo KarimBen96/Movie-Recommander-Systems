ad52c20e67fd0c18479a751edabfa634f551d6e0
unknown <fk_ben_hocine@esi.dz>
2021-08-04 00:49:44 +0100

    Revert "added load model in Model notebook"
    This reverts commit a6fd9a39abb7ff2c620d259a5cf954ffcc5293ca.

--- backend/MyAPI/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Movie
-
-
-class MovieSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ('title', 'genres')
-
--- backend/MyAPI/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
